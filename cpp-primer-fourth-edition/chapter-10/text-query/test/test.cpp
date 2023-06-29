cb71ffca902c17d0f0ecdcc515360999089ae3b9
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2023-06-30 00:55:14 +0500

    Add a positive test for testing TextQuery class.

---
+++ cpp-primer-fourth-edition/chapter-10/text-query/test/test.cpp
@@ -0,0 +1,13 @@
+#include "catch_amalgamated.hpp"
+#include "../include/text-query.hpp"
+
+TEST_CASE("Test TextQuery class") {
+    SECTION("Test query method") {
+        TextQuery text_query;
+        text_query.read_file("test/payloads/input.txt");
+        std::vector<size_t> expected_lines = {5, 8};
+
+        std::vector<size_t> lines = text_query.query("lorem"); // 0, 5, 8
+        REQUIRE(lines == expected_lines);
+    }
+}
